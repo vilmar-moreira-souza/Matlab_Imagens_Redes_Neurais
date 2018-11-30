59905c5d65c7af98d5576fade8aec1df3c2e671a
vilmar <vilmarmoreira2010@gmail.com>
2018-11-29 22:01:55 -0200

    rede neural

--- Rede_Neural.m
+++ Rede_Neural.m
@@ -1,63 +1,20 @@
-clc;
-clear all;
-%binarização das imagens
-a = rgb2gray(imread('quadrado1.png'));
-a1 = im2bw(a(:));
-b = rgb2gray(imread('quadrado2.png'));
-b1 = im2bw(b(:));
-c = rgb2gray(imread('quadrado3.png'));
-c1 = im2bw(c(:));
-d = rgb2gray(imread('quadrado.png'));
-d1 = im2bw(d(:));
-entrada = [c1;a1;b1];%dados de entrada
-padrao =  d1 ; % dados desejados de saida-alvo
-net = perceptron; % inicia a net  
-net.trainParam.goal = 1e-8; %erro final desejado
-net = configure(net,entrada,padrao);% configura a net com os dados inseridos
-net = train(net,entrada,padrao);%treina a net 
-quadrado = sim(net,entrada); 
-
-
-%------------------------------------------------------------------
-%------------------------------------------------------------------
-% circulo ---------------------------------------------------------
-
-
-%binarização das imagens
-A = rgb2gray(imread('circulo1.png'));
-A1 = im2bw(A(:));
-B = rgb2gray(imread('circulo2.png'));
-B1 = im2bw(B(:));
-C = rgb2gray(imread('circulo3.png'));
-C1 = im2bw(C(:));
-D = rgb2gray(imread('circulo.png'));
-D1 = im2bw(D(:));
-entrada = [A1;B1;C1];%dados de entrada
-padrao =  D1 ; % dados desejados de saida-alvo
-% Treina a net iterativamente, de 5 em 5 epochs,
-Nepoch = 5;
-NN = 20;
-net.trainParam.goal = 1e-8; %erro final desejado
-rede = perceptron; % inicia a net  
-rede = configure(rede,entrada,padrao);% configura a net com os dados inseridos
-rede = train(rede,entrada,padrao);%treina a net 
-circulo = sim(rede,entrada); 
-
-
-%-------------COMPARAÇÃO -------------------
-%   QUADRADO
-if(quadrado == d1)
-    disp('quadrado');
-else
-    disp('nao e quadrado');
-end
-%   CIRCULO
-if(circulo == D1)
-    disp('circulo');
-else
-    disp('nao e circulo');
-end
-
-
-
+clc;%limpa tela 
+clear all;%limpa variaveis
+%binarização das imagens 2 imagens de entrada
+img = rgb2gray(imread('quadro1.png'));
+img1 = im2bw(img);
+img2=vec2mat(img1,1049856);
+img = rgb2gray(imread('quadro2.png'));
+img1 = im2bw(img);
+img3=vec2mat(img1,1049856);
+%imagem de saida padrao/obejetivo
+img = rgb2gray(imread('quadro.png'));%Tons de cinza
+img1 = im2bw(img);%binario unica camada
+img4=vec2mat(img1,1049856);%converte em lihna com 768x1367=1049856, recomendavel usar imagens menores(50x50) 
+%EXECUÇAO------------------------------------------------------------------------------------------
+entrada = [img2;img3];%dados de entrada / 2 imagens convertidas em linha
+padrao =  img4; % dados desejados de saida-alvo /1 imagem convertida em linha
+co= 3;%camada oculta
+net = newff(entrada, padrao,co);
+rn = train(net,entrada,padrao);%treinamento da rede
 
--- Ubinarizacao.m
+++
@@ -1,6 +0,0 @@
-im = imread('quadrado.png');
-im2 = rgb2gray(im);
-im2 = im2bw(im2(:));
-figure, imshow(im2);
-x = uint8(-1*(double(im2)-255));
-figure, imshow(x);
