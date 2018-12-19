b578f5ab008af028b4a8202df8f6286268e8a376
vilmar <vilmarmoreira2010@gmail.com>
2018-12-18 23:54:32 -0200

    Update Rede_Neural.m

--- Rede_Neural.m
+++ Rede_Neural.m
@@ -4,9 +4,11 @@ clear all;%limpa variaveis
 img = rgb2gray(imread('quadro1.png'));
 img1 = im2bw(img);
 img2=vec2mat(img1,1049856);
+
 img = rgb2gray(imread('quadro2.png'));
 img1 = im2bw(img);
 img3=vec2mat(img1,1049856);
+
 %imagem de saida padrao/obejetivo
 img = rgb2gray(imread('quadro.png'));%Tons de cinza
 img1 = im2bw(img);%binario unica camada
@@ -15,6 +17,17 @@ img4=vec2mat(img1,1049856);%converte em lihna com 768x1367=1049856, recomendavel
 entrada = [img2;img3];%dados de entrada / 2 imagens convertidas em linha
 padrao =  img4; % dados desejados de saida-alvo /1 imagem convertida em linha
 co= 3;%camada oculta
-net = newff(entrada, padrao,co);
-rn = train(net,entrada,padrao);%treinamento da rede
+net = newff(entrada, padrao,co);%inicializa a rede 
+net = train(net,entrada,padrao);%treinamento da rede
 
+quadrado = sim(net,entrada);%testando a rede neural
+quadrado = uint8(quadrado);%transforma em inteiro
+
+
+%-------------COMPARAÇÃO -------------------
+%   QUADRADO
+if(quadrado== img4)
+    disp('quadrado');
+else
+    disp('nao e quadrado');
+end
